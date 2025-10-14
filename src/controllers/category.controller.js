00e8d1bfb479e4d093e0cbbe077066710dfe9077
Tuong Xuan Luong <133077787+xuanluongtuong@users.noreply.github.com>
2025-10-15 05:01:51 +0700

    fix category api

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -6,8 +6,5 @@ import Category from '../models/Category.js';
 // Get all categories
 export const getAllCategories = async (req, res) => {
     const categories = await Category.find();
-    if (categories.length === 0) {
-        throw new ErrorResponse(400, MESSAGE.CATEGORY_FETCH_FAILED);
-    }
     res.json(toResultOk({ msg: MESSAGE.CATEGORY_FETCH_SUCCESS, data: categories }));
 }
