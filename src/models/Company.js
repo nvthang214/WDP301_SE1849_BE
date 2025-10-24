b4794b1ac853cfac6f62a3b392dd9a01da869977
truongnnhe172873 <nntruong79230@gmail.com>
2025-10-24 23:05:40 +0700

    fix loi

--- src/models/Company.js
+++ src/models/Company.js
@@ -17,7 +17,6 @@ const CompanySchema = new mongoose.Schema({
     linkedin: { type: String },
     twitter: { type: String },
     youtube: { type: String },
-    instagram: { type: String },
   },
   contact: {
     email: { type: String },
