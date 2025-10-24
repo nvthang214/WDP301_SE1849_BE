f4fd1d69f8e6652923158255191121ba14be7ddf
truongnnhe172873 <nntruong79230@gmail.com>
2025-10-25 00:19:02 +0700

    fix lỗi middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -18,10 +18,9 @@ router.get("/profile/:userId", wrapAsync(getProfile));
 router.put("/profile/:userId", wrapAsync(updateProfile));
 
 // @route   GET /api/users/me
-// @desc    Get user profile by ID (for admins)
-// @access  Private (should be Admin)
-// Note: We should add an isAdmin middleware here in a real application
-router.get("/me", getMe);
+// @desc    Get current user's profile
+// @access  Private
+router.get("/me", authMiddleware, wrapAsync(getMe));
 
 // @route   GET /api/users/:id
 // @desc    Get user profile by ID (for admins)
